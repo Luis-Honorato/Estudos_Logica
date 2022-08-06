ddf8f61792117ce8390ca5f1eb9fedd3e76407b9
Luis Felipe Maciel Honorato <luis.honorato@faculdadesapiens.edu.br>
2022-08-06 16:47:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Logica_Progamacao_Dart
+ Estudo que tive para fundamentar minha lógica de progamação com Dart
---
+++ ex001.dart
@@ -0,0 +1,3 @@
+// crie um as variáveis produto1 = computador ; produto2 = tv ; preco1 = 2100.5; preco2 = 1830; idade = 30; codigo = 5291; genero = f;
+// produza a seguinte saida: produtos: o produto computdaro custa R$ 2100,50 ; o produto TV custa R$ 1830,00 ; codigo = 5291; dados da pessoa: genero F  e idade 30;
+
