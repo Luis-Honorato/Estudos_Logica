3b5e3cdbcb9e5712f0b949643291b0db3405992c
Luis Felipe Maciel Honorato <luis.honorato@faculdadesapiens.edu.br>
2022-08-08 20:41:56 -0400

    Create ex002.dart

---
+++ Extrutura_Sequencial/ex002.dart
@@ -0,0 +1,25 @@
+//Fazer um programa para ler as medidas da largura e comprimento de um terreno retangular com uma casa decimal, 
+// bem como o valor do metro quadrado do terreno com duas casas decimais.
+//Em seguida, o programa deve mostrar o valor da área do terreno, bem como o valor do preço do terreno, ambos com duas casas decimais
+import 'dart:io';
+
+void main(){
+  print('Digite as especificações do Terreno: ');
+  print('Largura: ');
+  String? Slargura = stdin.readLineSync();
+  double largura = double.parse(Slargura!);
+
+  print('Comprimento: ');
+  String? Scomprimento = stdin.readLineSync();
+  double comprimento = double.parse(Scomprimento!);
+
+  print('Valor do Metro quadrado em reais: ');
+  String? Svalor_metro = stdin.readLineSync();
+  double valor_metro = double.parse(Svalor_metro!);
+
+  double metro_quadrado = comprimento * largura;
+  double valor_terreno = metro_quadrado * valor_metro;
+
+  print('A área do terreno é de ${metro_quadrado}0m² e o valor é R${''} ${valor_terreno}0');
+
+}
